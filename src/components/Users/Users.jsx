6ff646267fcf426fb142e38c9c9e19c4d99a27a6
89041088851 <bogdan.kosnyrev@outlook.com>
2022-01-24 21:37:12 +0300

    add axios

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/ava.jpg'
+
+
+let Users = (props) => {
+
+  if (props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      props.setUsers(response.data.items)
+    })
+  }
+
+    return (
+      <div>
+        {
+          props.users.map(u => <div key={u.id}>
+          <span>
+            <div>
+              <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar"/>
+            </div>
+            <div>
+              {u.followed
+                ? <button onClick={() => {
+                  props.unfollow(u.id)
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                  props.follow(u.id)
+                }}>Follow</button>}
+            </div>
+          </span>
+            <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
+            </span>
+          </span>
+          </div>)
+        }
+      </div>
+    );
+  }
+  ;
+
+  export default Users;
