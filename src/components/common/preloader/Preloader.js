a482d77f11104bd909b4542ba292083b3ee70010
89041088851 <bogdan.kosnyrev@outlook.com>
2022-01-26 21:02:10 +0300

    added Preload component

---
+++ src/components/common/preloader/Preloader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import preloader from "../../../assets/images/Fidget-spinner.gif";
+
+export let Preloader = (props) => {
+  return (
+    <img src={preloader} alt='loader'/>
+  );
+};
